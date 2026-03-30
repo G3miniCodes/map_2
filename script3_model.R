18b53eba65a23bcb47eca288f4f0df6a68346351
Nikita Thakur <98184107+G3miniCodes@users.noreply.github.com>
2026-03-30 13:36:27 +0530

    Add interaction term to linear model

--- script3_model.R
+++ script3_model.R
@@ -1,10 +1,9 @@
-
-# Modeling Script
 data4 <- read.csv("old_table_4.csv")
 data5 <- read.csv("old_table_5.csv")
 
 df <- merge(data4, data5, by="id")
+df$interaction <- df$col_d * df$col_e
 
-model <- lm(col_d ~ col_e, data=df)
+model <- lm(col_d ~ col_e + interaction, data=df)
 
 summary(model)
