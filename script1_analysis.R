c0724ebc9d91d5ca4323e091a01e7475f284defa
Nikita Thakur <98184107+G3miniCodes@users.noreply.github.com>
2026-03-30 13:35:34 +0530

    Enhance analysis script with new calculations
    Added calculations for new columns and updated summary.

--- script1_analysis.R
+++ script1_analysis.R
@@ -1,8 +1,9 @@
-
-# Analysis Script 1
 data1 <- read.csv("old_table_1.csv")
 data2 <- read.csv("old_table_2.csv")
 
+data1$calc_a <- data1$col_a + data1$col_x
+data2$calc_b <- data2$col_b * data2$col_y
+
 merged_data <- merge(data1, data2, by="id")
 
-summary(merged_data)
+summary(merged_data[, c("col_a","col_x","col_b","col_y")])
