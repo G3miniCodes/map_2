a8ee2bc75199d81c02447283c75d9a31afd767e9
Nikita Thakur <98184107+G3miniCodes@users.noreply.github.com>
2026-03-30 13:36:02 +0530

    Update data cleaning script to include ratio calculation
    Refine data cleaning process by selecting specific columns and calculating a new ratio.

--- script2_cleaning.R
+++ script2_cleaning.R
@@ -1,7 +1,6 @@
-
-# Data Cleaning Script
 data3 <- read.csv("old_table_3.csv")
 
-clean_data <- na.omit(data3)
+clean_data <- na.omit(data3[, c("col_c","col_z")])
+clean_data$ratio <- clean_data$col_c / clean_data$col_z
 
 write.csv(clean_data, "clean_old_table_3.csv", row.names=FALSE)
