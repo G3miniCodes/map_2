d3064d1be93a57c00eb8aa219549633087b7881b
Nikita Thakur <98184107+G3miniCodes@users.noreply.github.com>
2026-03-30 13:36:48 +0530

    Add total column and modify print output in report

--- script4_report.R
+++ script4_report.R
@@ -1,9 +1,8 @@
-
-# Reporting Script
 data6 <- read.csv("old_table_6.csv")
 data7 <- read.csv("old_table_7.csv")
 data8 <- read.csv("old_table_8.csv")
 
 combined <- rbind(data6, data7, data8)
+combined$total <- combined$col_f + combined$col_g + combined$col_h
 
-print(head(combined))
+print(head(combined[, c("col_f","col_g","col_h")]))
